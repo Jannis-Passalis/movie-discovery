aadbacec4206b3896ee222701c26319053258ebb
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-10-15 14:01:15 +0200

    NavBar made

---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              activeStyle={{
+                fontWeight: "bold",
+                color: "red",
+              }}
+              exact
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              activeStyle={{
+                fontWeight: "bold",
+                color: "red",
+              }}
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/discover"
+              activeStyle={{
+                fontWeight: "bold",
+                color: "red",
+              }}
+            >
+              Discover Movies
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
